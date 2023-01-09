ad5990c741c53f902f85b836dfc0c03419cf8297
Ngozi Okafor <ngoziokafor2014@gmail.com>
2023-01-09 15:52:49 +0100

    commit recent changes

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const toogleReadMore = (event)=>{
-	const read_more_text = document.querySelector("#more-text")
-	read_more_text_style = window.getComputedStyle(read_more_text)
-	if(read_more_text_style.getPropertyValue('display') === 'none'){
-		event.target.textContent = 'Read less'
-		read_more_text.style.display = 'block'
-		document.querySelector("#three-dots").style.display = 'none'
-	}else{
-		event.target.textContent = 'Read more'
-		read_more_text.style.display = 'none'
-		document.querySelector("#three-dots").style.display = 'inline'
-	}
-
-}
+const toogleReadMore = (event) => {
+  const read_more_text = document.querySelector('#more-text');
+  read_more_text = window.getComputedStyle(read_more_text);
+  if (read_more_text.getPropertyValue('display') === 'none') {
+    event.target.textContent = 'Read less';
+    read_more_text.style.display = 'block';
+    document.querySelector('#three-dots').style.display = 'none';
+  } else {
+    event.target.textContent = 'Read more';
+    read_more_text.style.display = 'none';
+    document.querySelector('#three-dots').style.display = 'inline';
+  }
+};
 
 /*
 
